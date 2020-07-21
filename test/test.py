115b35dc34f5e74b6b3f7e60e5efa9914b176adf
Tomer Lev Lehman <tomer@h-MacBook-Pro-sl-Tomer.local>
2020-07-21 23:26:11 +0300

    minQ

--- test/test.py
+++ test/test.py
@@ -11,8 +11,8 @@ red_exp_out_0 = "reduction/exp_out_0.txt"
 
 class MyTestCase(unittest.TestCase):
 
-	# def test_0(self):
-	# 	self.assertEqual(solve(in_0), Solution.from_txt(exp_out_0))
+	def test_0(self):
+		self.assertEqual(solve(in_0), Solution.from_txt(exp_out_0))
 
 	def test_reduction(self):
 		self.assertEqual(Graph.from_text(red_in_0), Graph.from_text(red_exp_out_0))
